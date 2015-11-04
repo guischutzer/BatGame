6165b75cc91f78ee616c8eddfa1c1a1b062710e1
Guilherme Schützer <guischutzer@gmail.com>
2015-11-04 16:56:52 -0200

    Pulo gruda só um pouquinho -- há distinção correta no canto

--- love/main.lua
+++ love/main.lua
@@ -18,7 +18,7 @@ function love.load()
 	map = loader.load("level.tmx")
 
 	-- load HardonCollider, set callback to on_collide and size of 100
-	collider = HC(100, on_collide)
+	collider = HC(128, on_collide)
 
 	-- find all the tiles that we can collide with
 	allSolidTiles = findSolidTiles(map)
@@ -30,8 +30,14 @@ function love.load()
     x = -1000,
     y = -1000
   }
+  sum_x = 0
+  sum_y = 0
+  x_history = {0, 0, 0, 0}
+  y_history = {0, 0, 0, 0}
 
   gravity = (hero.y_speed_base^2)/(2 * hero.jump_height)
+  move = {x=0, y=0}
+  i = 0
 
 end
 
@@ -40,13 +46,11 @@ function love.update(dt)
 	-- do all the input and movement
 
 	handleInput(dt)
-	updateHero(dt)
 
 	-- update the collision detection
 
-
+	updateHero(dt)
 	collider:update(dt)
-
 
 
 end
@@ -61,6 +65,8 @@ function love.draw()
 
 	-- draw the hero as a rectangle
 	hero:draw("fill")
+
+  love.graphics.print(hero.y_speed)
 end
 
 function on_collide(dt, shape_a, shape_b, mtv_x, mtv_y)
@@ -83,14 +89,74 @@ function collideHeroWithTile(dt, shape_a, shape_b, mtv_x, mtv_y)
 		return
 	end
 
-  hero_shape.air = false
-  hero_shape.y_speed = 0
+
+  table.insert(x_history, 4, mtv_x)
+  table.remove(x_history, 1)
+
+  table.insert(y_history, 4, mtv_y)
+  table.remove(y_history, 1)
+
+  sum_x = 0
+  -- io.write("x_history = {")
+  for i,val in ipairs(x_history) do
+    if val > 0 then
+      sum_x = sum_x + val
+    else
+      sum_x = sum_x - val
+    end
+    --io.write(val)
+    -- if i ~= 4 then io.write(", ") end
+  end
+  -- io.write("}\n")
+
+  sum_y = 0
+  -- io.write("y_history = {")
+  for i,val in ipairs(y_history) do
+    if val > 0 then
+      sum_y = sum_y + val
+    else
+      sum_y = sum_y - val
+    end
+    -- io.write(val)
+    -- if i ~= 4 then io.write(", ") end
+  end
+  -- io.write("}\n")
+
+  --print("------------------------------")
+  -- print("sum_x = " .. sum_x .. " mtv_x = " .. mtv_x .. "\nsum_y = " .. sum_y .. " mtv_y = " .. mtv_y) -- " mtv_x = " .. mtv_x .. " mtv_y = " .. mtv_y)
+  if mtv_y < 0 then
+    if sum_x + old.x <= 4 -- no canto, esse valor não ultrapassa 4!!
+    and sum_x >= 0 then
+      hero_shape.air = false
+      hero_shape.y_speed = 0
+    end
+  elseif mtv_y > 0 then
+    print("opa")
+    hero_shape.y_speed = 0
+  end
+
+  if sum_x >= 0.5 then hero.speed_x = 0 end
+
+  -- if hero.air == true then
+  --   print("lnx = " .. last_nonzero_x .. " ON hero.air")
+  -- else
+  --   print("lnx = " .. last_nonzero_x)
+  -- end
+
+  -- if mtv_x > 0 then
+  --   hero.l_wall = true
+  -- elseif mtv_x < 0 then
+  --   hero.r_wall = true
+  -- end
 
 	-- why not in one function call? because we will need to differentiate between the axis later
+
   if mtv_x ~= old.x then hero_shape:move(mtv_x, 0) end
   if mtv_y ~= old.y then hero_shape:move(0, mtv_y) end
+
   old.x = mtv_x
   old.y = mtv_y
+
 
 end
 
@@ -106,6 +172,8 @@ function setupHero(x,y)
   hero.y_speed_base = -400
   hero.y_speed = 0
   hero.air = true
+  hero.l_wall = false
+  hero.r_wall = false
 
   hero.jump_height = 100
   hero.pode_pular = true
@@ -132,7 +200,7 @@ function handleInput(dt)
   end
 
 	if love.keyboard.isDown("left") then
-    flip = 1
+    flip = true
     if hero.x_speed >= 0 then
       if hero.x_speed >= hero.x_speed_base then
         hero.x_speed = -hero.x_speed
@@ -160,19 +228,22 @@ function handleInput(dt)
   else
     hero.x_speed = 0
   end
+
+
 end
 
 function updateHero(dt)
 
-  if hero.air then -- we're falling
-		hero.y_speed = hero.y_speed + gravity * dt
-    hero:move(0, dt * hero.y_speed)
-	end
 
-  print(hero.x_speed)
-  hero:move(hero.x_speed*dt, 0)
+
+  if hero.air == true then -- we're falling
+    hero.y_speed = hero.y_speed + gravity * dt
+    hero:move(0, hero.y_speed*dt)
+  end
 
   hero.air = true
+
+  hero:move(hero.x_speed*dt, 0)
 
 end
 
